7db16d8d6611b262e206bf290fde943e23be6e32
Gus-Victrix <iamfrsncis@gmail.com>
2023-09-21 22:43:36 +0300

    ADDED insertion sort algorithm function for doubly linked lists.

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Applies the insertion sort algorithm to sort a list.
+ *
+ * @list: List of integers.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp, *traverse;
+	char moved = 0;
+
+	if (!list)
+		return;
+	traverse = (*list)->next;
+	current = traverse;
+
+	while(current)
+	{
+		traverse = current->next;
+		temp = current->prev;
+		moved = 0;
+		while (temp->prev != NULL && temp->n > current->n)
+		{
+			temp = temp->prev;
+			moved = 1;
+		}
+		if (moved)
+		{
+			if (current->next)
+				current->next->prev = current->prev;
+			current->prev->next = current->next;
+			if (temp->prev == NULL)
+			{
+				temp->prev = current;
+				current->next = temp;
+				current->prev = NULL;
+				*list = current;
+			}
+			else
+			{
+				current->prev = temp;
+				current->next = temp->next;
+				temp->next->prev = current;
+				temp->next = current;
+			}
+		}
+		current = traverse;
+		print_list(*list);
+	}
+	return;
+}
