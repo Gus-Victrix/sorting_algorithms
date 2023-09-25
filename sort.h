e30682d8705a06c1e3113661ab9a25a0a56ec110
Gus-Victrix <iamfrsncis@gmail.com>
2023-09-25 12:04:53 +0300

    Added comment desctiption for struct elem_s

--- sort.h
+++ sort.h
@@ -15,11 +15,17 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct elem_s - Data container for an array index and it's value.
+ *
+ * @n: The value stored at the index.
+ * @index: The array index.
+ */
 typedef struct elem_s
 {
 	int n;
@@ -34,6 +40,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void cocktail_sort_list(const listint_t **list);
+void cocktail_sort_list(listint_t **list);
 
 #endif
